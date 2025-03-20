51190fcd0314eb10b007b68ec0573c92a3fe8bb6
solaceVA <AlwinShane1@gmail.com>
2025-03-20 12:28:10 +0530

    change the hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "I am Alwin!\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
